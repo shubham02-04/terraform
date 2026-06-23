5002416ad6c3618d5a5d29a2a34f352a6754177d
shubham02-04 <shubham.mgujarati@gmail.com>
2026-06-23 18:09:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,4 @@
 terraform {
- backend "azurerm" {
-      resource_group_name  = "Demo-Shubham-resources"
-      storage_account_name = "terraformstorage23626"
-      container_name       = "terraform-tfstate-files"
-      key                  = "demo.terraform.tfstate"
+  backend "azurerm" {
   }
 }
-
